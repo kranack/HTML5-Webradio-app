809c0df733789863ea88d47495c882894d7f17d9
Damien Calesse <madeinjb@gmail.com>
2014-07-14 16:53:50 +0200

    Test Crossdomain

---
+++ app/javascript/functions.js
@@ -0,0 +1,14 @@
+/************************************
+ *
+ *	@file=functions.js
+ *	@author=Damien Calesse
+ *
+ ************************************/
+
+function setToLowerCase (str) {
+	return (str) ? str.toLowerCase() : '';
+}
+
+function displayDebug(message) {
+	return (DEBUG) ? console.debug(message) : false;
+}
